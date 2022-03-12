79c447f8f90c625c298245f679d5917a7bc2f106
unknown <gmgustavom@gmail.com>
2022-03-12 02:40:11 -0300

    feat: showing Hints

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -23,6 +23,7 @@ export default class Board extends Component {
           isMine: false,
           isClicked: false,
           isFlagged: false,
+          bombsAround: 0,
         }));
       }
       newBoard.push(boardRow);
@@ -43,7 +44,54 @@ export default class Board extends Component {
       if (!board[mineY][mineX].isMine) { 
         plantedmines += 1;
         board[mineY][mineX].isMine = true;
+        console.log(mineY, mineX)
+        this.giveHint(mineY, mineX);
       }
+    }
+  }
+
+  giveHint = (mineY, mineX) => {
+    const { board } = this.state;
+    const { width, heigth } = this.props;
+    // Top Left
+    if (mineY > 0 && mineX < 0) {
+      console.log( board[mineY - 1][mineX - 1]);
+      board[mineY - 1][mineX - 1].bombsAround += 1;
+    }
+
+    // Top Center
+    if (mineY > 0) {
+      board[mineY - 1][mineX].bombsAround += 1;
+    }
+
+    // Top Right
+    if (mineY > 0 && mineX < width - 1) {
+      board[mineY][mineX].bombsAround += 1;
+    }
+
+    // Left
+    if (mineX > 0) {
+      board[mineY][mineX - 1].bombsAround += 1;
+    }
+
+    // Right
+    if (mineX < width - 1) {
+      board[mineY][mineX + 1].bombsAround += 1;
+    }
+
+    // Bottom Left
+    if (mineY < heigth - 1 && mineX > 0) {
+      board[mineY + 1][mineX - 1].bombsAround += 1;
+    }
+
+    // Bottom
+    if (mineY < heigth - 1) {
+      board[mineY + 1][mineX].bombsAround += 1;
+    }
+
+    // Bottom Right
+    if (mineY < heigth - 1 && mineX < width - 1) {
+      board[mineY + 1][mineX + 1].bombsAround += 1;
     }
   }
 
